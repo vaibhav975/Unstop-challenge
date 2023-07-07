68c97cf425c0f6d7fa5142eef2f43131003d840a
vaibhav975 <vaibhavvishal277@gmail.com>
2023-07-07 16:13:10 +0530

    logic part added

--- src/app/coach/coach.component.ts
+++ src/app/coach/coach.component.ts
@@ -7,68 +7,151 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coach.component.css']
 })
 export class CoachComponent {
-  seats: number[][] = [];
-  n: number = 0;
-  message: string = '';
-
-
-  constructor(private http: HttpClient) {
-    this.getSeatStatus();
+  seats: any[][] = [];
+  selectedSeats: any[] = [];
+  booking:number = 0;
+  constructor() {
+    this.initializeSeats();
   }
 
-  bookSeats() {
-  if(this.n>7){
-    this.message = 'Seats more than 7 not allowed';
-
-    return;
-  }
-  for(let i=0;i<7;i++){
-    for(let j=0;j<7;j++){
-      if(this.seats[i][j]==0){
-        this.seats[i][j]=1;
-        this.n--;
-        if(this.n==0){
-          this.message = 'Booked';
-          return;
-        }
+  
+  initializeSeats(): void {
+    const rows = 12;
+    const seatsPerRow = 7;
+    const lastRowSeats = 3;
+    const totalSeats = (rows - 1) * seatsPerRow + lastRowSeats;
+    
+    let seatNumber = 1;
+    for (let i = 0; i < rows - 1; i++) {
+      const row = [];
+      for (let j = 0; j < seatsPerRow; j++) {
+        row.push({ number: seatNumber, booked: false, selected: false });
+        seatNumber++;
       }
-      else{
-        this.seats[i][j]=-1;;
+      this.seats.push(row);
+    }
+    
+    const lastRow = [];
+    for (let j = 0; j < lastRowSeats; j++) {
+      lastRow.push({ number: seatNumber, booked: false, selected: false });
+      seatNumber++;
+    }
+    this.seats.push(lastRow);
+  }
+
+  availableSeats(): number {
+    return this.seats.reduce((acc, row) => {
+      return acc + row.filter(s => !s.booked).length;
+    }, 0);
+  }
+
+  
+  reserveSeat(seat: any): void {
+    if (seat.booked) {
+      return;
+    }
+    
+    seat.selected = !seat.selected;
+    
+    if (seat.selected) {
+      this.selectedSeats.push(seat);
+    } else {
+      const index = this.selectedSeats.findIndex(s => s.number === seat.number);
+      if (index !== -1) {
+        this.selectedSeats.splice(index, 1);
       }
     }
-
   }
-  this.message = 'Booked';
-  this.seats = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0],
-  ];
+  
+  reserveSeats(): void {
+    let availableSeats = this.availableSeats();
+    if(availableSeats < this.booking){
+      alert("Please select less than or equal to "+availableSeats+" seats");
+      return;
+    }
+    console.log(availableSeats,"availableSeats");
+    if (this.booking === 0) {
+      return;
+    }
+    
+    if (this.booking > 7) {
+      alert('You can reserve up to 7 seats at a time.');
+      return;
+    }
+    
+    let reservedSeats: any[] = [];
+    
+    if (this.reserveSeatsInRow(this.booking, reservedSeats)) {
+      this.updateSeats(reservedSeats);
+    } else {
+      this.reserveSeatsNearby(this.booking, reservedSeats);
+      this.updateSeats(reservedSeats);
+    }
+    
+    this.clearSelectedSeats();
+  }
+  
+  reserveSeatsInRow(numSeats: number, reservedSeats: any[]): boolean {
+    for (const row of this.seats) {
+      let startIndex = -1;
+      for (let i = 0; i <= row.length - numSeats; i++) {
+        let count = 0;
+        for (let j = i; j < i + numSeats; j++) {
+          if (!row[j].booked) {
+            count++;
+          }
+        }
+        if (count === numSeats) {
+          startIndex = i;
+          break;
+        }
+      }
+      
+      if (startIndex !== -1) {
+        for (let i = startIndex; i < startIndex + numSeats; i++) {
+          reservedSeats.push(row[i]);
+        }
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  reserveSeatsNearby(numSeats: number, reservedSeats: any[]): void {
+    const seatsToReserve = numSeats - reservedSeats.length;
+    
+    for (const row of this.seats) {
+      for (let i = 0; i < row.length; i++) {
+        if (!row[i].booked) {
+          reservedSeats.push(row[i]);
+          
+          if (reservedSeats.length === numSeats) {
+            return;
+          }
+        }
+      }
+    }
+  }
+  
+  updateSeats(reservedSeats: any[]): void {
+    for (const seat of reservedSeats) {
+      seat.booked = true;
+    }
+  }
+  
+  clearSelectedSeats(): void {
+    for (const seat of this.selectedSeats) {
+      seat.selected = false;
+    }
+    
+    this.selectedSeats = [];
   }
 
-  getSeatStatus() {
-    this.seats = [
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0],
-    ];
-   }
-}
+  reset(){
+    this.seats = [];
+    this.initializeSeats();
+    this.clearSelectedSeats();
+  }
+ }
+
